ff690e01022c3143791f0fb2a6dc20617b96aa64
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-22 10:25:16 -0500

    updated CSS file; added react icons; structured App.js return

--- src/Review.js
+++ src/Review.js
@@ -1,4 +1,4 @@
-mport React, { useState } from 'react';
+import React, { useState } from 'react';
 import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
