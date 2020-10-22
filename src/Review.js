c392cf7f11e0de9376fa5fbb1f40e821296b39f9
Phillipe Rodriguez <philliperod@gmail.com>
2020-10-22 11:04:17 -0500

    created an useState hook with a default value of zero because the value will be an array and should show the first person or index; destructured the props and equal it to the people array with the state value; this will render specifically to that individual when chosen

--- src/Review.js
+++ src/Review.js
@@ -3,7 +3,23 @@ import people from './data';
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+      </div>
+    </article>
+  );
 };
 
 export default Review;
+
+// first setup a hook with a default value of zero because the value is an array and to showcase the first person you start with index zero
+// now how can you access each individual or properties?
+// meaning you only want to render one person
+// you can access the properties using destructuring from the people array and specify which item you are looking for
+// you can use the state value to do so
+// destructuring the array and equal it to the array which holds the state value so whoever you choose will showcase those properties
